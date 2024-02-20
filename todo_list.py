bba9f69b1e83d5f523d8ff987d5a26ccec00b4fa
Isaac Rounds <rounds.isaac@gmail.com>
2024-02-19 22:25:42 -0500

    changed task storage from dictionary to list

--- todo_list.py
+++ todo_list.py
@@ -4,26 +4,28 @@ Created on Mon Feb 19 20:13:51 2024
 
 @author: round
 """
-tasks_dic = {}
+tasks_list = []
 
 
 def Add_new(tasks):
     new_task = input("What is the new task? :")
-    dic_length = len(tasks)
-    tasks[dic_length + 1] = new_task
+    tasks.append(new_task)
     return tasks
 
 def View_tasks(tasks):
     for i in range(1, len(tasks) + 1):  
-        print(f"Task {i}: {tasks[i]}")
+        print(f"Task {i}: {tasks[i-1]}")
     return
 
 def task_complete(tasks):
-    
+    View_tasks(tasks)
+    completed = int(input("Which tasks did you complete? :"))
+    del tasks[completed - 1]
     return tasks
 
-tasks_dic = Add_new(tasks_dic)
-tasks_dic = Add_new(tasks_dic)
-View_tasks(tasks_dic)
-View_tasks(tasks_dic)
+tasks_list = Add_new(tasks_list)
+tasks_list = Add_new(tasks_list)
+View_tasks(tasks_list)
+tasks_list = task_complete(tasks_list)
+View_tasks(tasks_list)
 
